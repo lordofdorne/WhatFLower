5d2a55fb5ddae3bcad7621ef012c37cecaf1a7f1
asim samuel <asim.samuel1@gmail.com>
2019-12-07 19:08:34 -0500

    WhatFlower
    Added Cocoapods Alamofire and SwiftyJSON

--- WhatFLower/ViewController.swift
+++ WhatFLower/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]  as? UIImage {
+        
+            guard  let convertedCIImage = CIImage(image: userPickedImage) else {
+                
+                fatalError("cannot convert to CIImage.")
+            }
+        detect(image: convertedCIImage)
+       
+            imageView.image = userPickedImage
+        
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
+    func detect(image: CIImage) {
+        
+        guard  let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("cannot import model")
+        }
+        let request = VNCoreMLRequest(model: model)  { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+       
+        do {
+        try handler.perform([request])
+        }
+        catch {
+            print(error)
+            }
+        }
+    
+   
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
